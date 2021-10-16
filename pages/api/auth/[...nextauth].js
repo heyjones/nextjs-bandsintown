1a8bd068d337cda7b2230de298d85695abb6858b
Chris Jones <chris@heyjones.com>
2021-10-16 10:31:57 -0700

    fix for spotify scope and jwt / session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,8 +11,26 @@ export default NextAuth({
         SpotifyProvider({
             clientId: process.env.SPOTIFY_CLIENT_ID,
             clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-            scope: 'user-read-email user-follow-read'
+            authorization: {
+                params: {
+                    scope: 'user-read-email user-follow-read'
+                }
+            }
         })
     ],
+    callbacks: {
+        async session({ session, token, user }) {
+          // Send properties to the client, like an access_token from a provider.
+          session.accessToken = token.accessToken
+          return session
+        },
+        async jwt({ token, account }) {
+            // Persist the OAuth access_token to the token right after signin
+            if (account) {
+                token.accessToken = account.access_token
+            }
+            return token
+        }
+    },
     debug: false
 })
